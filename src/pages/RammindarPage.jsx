3b10ec09ec0df8aff3541543d84b586051f56c29
Ankita ranjan <contact.ankita1499@gmail.com>
2024-03-30 16:20:34 +0530

    dfdf

--- src/pages/RammindarPage.jsx
+++ src/pages/RammindarPage.jsx
@@ -7,14 +7,14 @@ import {Helmet} from "react-helmet";
 export default function RammindarPage() {
   return (
     <div>
-       <Helmet>
+       <head>
        <title>Turbo Todo</title>
        <meta property="og:type"               content="article" />
           <meta property="og:title"              content="titulo para el home de la tienda" />
           <meta property="og:description"        content="esta es la decripcion del home de la tienda" />
           <meta property="og:image"              content="https://media.gettyimages.com/photos/bogota-at-sunset-picture-id107069344?s=612x612" />
 
-            </Helmet>
+            </head>
       <Navbar/>
        <Rammindar/>
        <Footer/>
