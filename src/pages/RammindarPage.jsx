a749dca0cf62bed397637c8f44e410b07fa313b7
Ankita ranjan <contact.ankita1499@gmail.com>
2024-04-03 14:42:11 +0530

    dfdfdfdsfdfddvfgfgf

--- src/pages/RammindarPage.jsx
+++ src/pages/RammindarPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
-import Rammindar from '../components/discription/Rammandir'
+import Rammindar from '../components/description/Rammandir'
 export default function RammindarPage() {
   return (
     <div>
